1b3b805b8acbed8c0f699faa7320dfed2082893c
Rustem <ss.rustem@gmail.com>
2022-09-08 23:34:32 +0200

    added tests
    folder structure refactoring

--- project-text-and-summary/app/db/session.py
+++ project-text-and-summary/app/db/session.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///example.db"
+from app.core.config import settings
 
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URI,
+    settings.SQLALCHEMY_DATABASE_URI,
     # required for sqlite
     connect_args={"check_same_thread": False},
 )
---
+++ project-text-and-summary/app/tests/conftest.py
@@ -0,0 +1,42 @@
+from typing import Generator
+
+import pytest
+
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.api import deps
+from app.db.base import Base
+from app.main import app
+from app.tests.factories import DocumentFactory
+
+
+TEST_SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
+engine = create_engine(TEST_SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+Session = sessionmaker()
+
+Base.metadata.create_all(bind=engine)
+
+@pytest.fixture(scope='module')
+def connection():
+    connection = engine.connect()
+    yield connection
+    connection.close()
+
+@pytest.fixture(scope='function')
+def session(connection):
+    transaction = connection.begin()
+    session = Session(autocommit=False, autoflush=False, bind=connection)
+    DocumentFactory._meta.sqlalchemy_session = session
+    yield lambda: (yield session)
+    session.close()
+    transaction.rollback()
+
+
+@pytest.fixture()
+def client(session) -> Generator:
+    with TestClient(app) as client:
+        app.dependency_overrides[deps.get_db] = session
+        yield client
+        app.dependency_overrides = {}
